793a09830fb6cea638b75744af1d3b8f12639f47
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-27 17:50:36 +0800

    Add user table and styling

--- user-management/src/components/UserComponent.tsx
+++ user-management/src/components/UserComponent.tsx
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Users } from '../type';
 
 interface Props {
@@ -6,10 +8,20 @@ interface Props {
 }
 
 const UserComponent = (props: Props) => {
+
     return (
-        <div>
-            {props.user?.firstName}
-        </div>
+        <>
+            {props.user ? 
+            <tr key={props.user.id}>
+                    <td>{props.user.firstName + ' ' + props.user.lastName}</td>
+                    <td>{props.user.email}</td>
+                    <td>{moment(new Date(props.user.dob)).format('DD MMM YYYY')}</td>
+                    <td className="d-flex justify-content-around">
+                        <Button variant="success">Update</Button>
+                        <Button variant="danger">Delete</Button>
+                    </td>
+            </tr> : ''}
+        </>
     );
 }
 
--- user-management/src/container/UserContainer.tsx
+++ user-management/src/container/UserContainer.tsx
@@ -1,21 +1,59 @@
-import React from 'react';
-import SearchComponent from '../components/SearchComponent';
-import UserComponent from '../components/UserComponent';
-import { Users } from '../type';
+import React from "react";
+import { Alert } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import SearchComponent from "../components/SearchComponent";
+import UserComponent from "../components/UserComponent";
+import { Users } from "../type";
 
 interface Props {
-    users: Users[] | undefined
+    users: Users[] | undefined;
 }
 
 const UserContainer = (props: Props) => {
     return (
         <div>
-            <SearchComponent/>
-            {props.users ? props.users.map((user) => {
-                return <UserComponent key={user.id} user={user}/>
-            }) : ''}
+            <SearchComponent />
+            {props.users && props.users.length !== 0 ? (
+                <Table
+                    striped
+                    bordered
+                    hover
+                    style={{
+                        marginTop: "20px",
+                    }}
+                >
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Date of Birth</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.users
+                            ? props.users.map((user) => {
+                                    return (
+                                        <UserComponent
+                                            key={user.id}
+                                            user={user}
+                                        />
+                                    );
+                            })
+                            : ""}
+                    </tbody>
+                </Table>
+            ) : (
+                <Alert
+                    style={{
+                        marginTop: "20px",
+                    }}
+                >
+                    {"There are no users created yet."}
+                </Alert>
+            )}
         </div>
     );
-}
+};
 
 export default UserContainer;
